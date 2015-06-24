5a06a65d138228e9a8a2d97c13df54e4c820c994
Holger Karl <hkarl@ieee.org>
2015-06-24 20:32:52 +0200

    fix branding

--- UPB/urls.py
+++ UPB/urls.py
@@ -20,6 +20,7 @@ from django.contrib.auth.decorators import login_required, user_passes_test
 from django.contrib.auth import REDIRECT_FIELD_NAME
 from django.views.generic import TemplateView
 
+admin.site.site_header = 'UPB Webanwendungen'
 
 def active_and_login_required(function=None,
                               redirect_field_name=REDIRECT_FIELD_NAME,
