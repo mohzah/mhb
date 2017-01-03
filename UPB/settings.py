799ff8aa761a5a4e84b51a3a0ee830f8a5e057fc
root <root@tunis.uni-paderborn.de>
2017-01-03 11:55:34 +0100

    adding allowed hosts to django hosts

--- UPB/settings.py
+++ UPB/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'k^jz(c2b@nbt@e#(t&&qy@r0f8lh3a$s5eh)k3z0q=hp$3a$^&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [u'modulhandbuch-mb.uni-paderborn.de',]
 
 
 # Application definition
